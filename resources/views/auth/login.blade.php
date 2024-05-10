643c2fc077c4a6cbcf15cbfe10eae94b11bc295c
phuocpham12495 <phuocpham12495@gmail.com>
2024-05-11 04:53:54 +0700

    deploy dev phuocit95 0511240453

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,10 +6,15 @@
     <form method="POST" action="/login">
         @csrf
 
-        <div class="space-y-12">
-            <div class="border-b border-gray-900/10 pb-12">
-                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                    <div class="sm:col-span-4">
+        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Log in to your
+                    account</h2>
+            </div>
+
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form class="space-y-6" action="#" method="POST">
+                    <div>
                         <x-form-label for="email">Email</x-form-label>
                         <div class="mt-2">
                             <x-form-input type="email" name="email" id="email" :value="old('email')" required />
@@ -17,21 +22,29 @@
                         </div>
                     </div>
 
-                    <div class="sm:col-span-4">
+                    <div>
                         <x-form-label for="password">Password</x-form-label>
                         <div class="mt-2">
                             <x-form-input type="password" name="password" id="password" required />
                             <x-form-error name="password" />
                         </div>
                     </div>
-                </div>
+
+                    <div>
+                        <button type="submit"
+                            class="mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Log
+                            in</button>
+                    </div>
+                </form>
+
+                <p class="mt-10 text-center text-sm text-gray-500">
+                    Not a member?
+                    <a href="/register"
+                        class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Register</a>
+                </p>
             </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <a type="button" class="text-sm font-semibold leading-6 text-gray-900" href="/">Cancel</a>
-            <x-form-button>Log In</x-form-button>
-        </div>
     </form>
 
 </x-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
     <form method="POST" action="/register">
         @csrf
 
-        <div class="space-y-12">
+        {{-- <div class="space-y-12">
             <div class="border-b border-gray-900/10 pb-12">
                 <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
@@ -48,6 +48,59 @@
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <a type="button" class="text-sm font-semibold leading-6 text-gray-900" href="/">Cancel</a>
             <x-form-button>Register</x-form-button>
+        </div> --}}
+
+        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Register your
+                    account</h2>
+            </div>
+
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form class="space-y-6" action="#" method="POST">
+                    <div>
+                        <x-form-label for="name">Name</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input type="text" name="name" id="name" :value="old('name')" required />
+                            <x-form-error name="name" />
+                        </div>
+                    </div>
+                    <div>
+                        <x-form-label for="email">Email</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input type="email" name="email" id="email" :value="old('email')" required />
+                            <x-form-error name="email" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <x-form-label for="password">Password</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input type="password" name="password" id="password" required />
+                            <x-form-error name="password" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <x-form-label for="password_confirmation">Retype Password</x-form-label>
+                        <div class="mt-2">
+                            <x-form-input type="password" name="password_confirmation" id="password_confirmation"
+                                required />
+                            <x-form-error name="password_confirmation" />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button type="submit"
+                            class="mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
+                    </div>
+                </form>
+
+                <p class="mt-10 text-center text-sm text-gray-500">
+                    Already a member?
+                    <a href="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Login</a>
+                </p>
+            </div>
         </div>
     </form>
 
