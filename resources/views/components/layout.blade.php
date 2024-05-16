d4747cbc0059814186989609aeb48487f5426535
phuocpham12495 <phuocpham12495@gmail.com>
2024-05-16 17:33:17 +0700

    fix vite not working on host phuocit95

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,8 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} - PhuocIt95</title>
+    <link rel="stylesheet" href="/build/assets/app-Cm8WSGZV.css">
     {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 
@@ -22,6 +24,8 @@
         </main>
         <x-footer></x-footer>
     </div>
+
+    <script src="/build/assets/app-QJMq9KiZ.js"></script>
 </body>
 
 </html>
